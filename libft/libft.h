3454a5bcaeb818a141e6a7f6500188d6dc0cc26e
ali <137689491+alikosaca@users.noreply.github.com>
2025-03-08 11:59:15 +0300

    init

---
+++ libft/libft.h
@@ -0,0 +1,23 @@
+# ifndef LIBFT_H
+#define LIBFT_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+void	ft_putnbr_fd(int n, int fd);
+char	*ft_itoa(int n);
+int		ft_atoi(const char *str);
+void	*ft_calloc(size_t nelem, size_t elsize);
+int		ft_count_c(char *s, char c);
+int		ft_count_lines(int fd, int line_length, int img_w);
+int		ft_line_length(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, const char *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+
+
+#endif
